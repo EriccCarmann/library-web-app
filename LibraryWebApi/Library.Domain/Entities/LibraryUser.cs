a12830c471a76389b2c5e791ff32a46103544301
Sasha Manyonok <83176347+EriccCarmann@users.noreply.github.com>
2024-11-15 18:22:34 +0300

    Update LibraryUser.cs

--- LibraryWebApi/Library.Domain/Entities/LibraryUser.cs
+++ LibraryWebApi/Library.Domain/Entities/LibraryUser.cs
@@ -5,7 +5,5 @@ namespace Library.Domain.Entities
     public class LibraryUser : IdentityUser
     {
         public List<Book>? Books { get; } = null;
-        public string? RefreshToken { get; set; }
-        public DateTime RefreshTokenExpiry { get; set; }
     }
 }
